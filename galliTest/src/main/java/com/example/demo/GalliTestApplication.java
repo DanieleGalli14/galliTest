77455564422559a46f4c6c7ec164dd1cb187f04e
DanieleGalli14 <galli.daniele14@gmail.com>
2021-11-08 10:15:57 +0100

    First Commit ed inserimento del progetto nella Repository

---
+++ galliTest/src/main/java/com/example/demo/GalliTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalliTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalliTestApplication.class, args);
+	}
+
+}
